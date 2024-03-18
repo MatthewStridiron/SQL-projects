b2b818b95cd17d1bbbe2b3d4525628b9381577a0
Matthew Stridiron <mstridiron.ptech@gmail.com>
2024-03-18 10:17:39 -0500

    Update yammer-sudden-drop-in-users.sql

--- Yammer - Sudden Drop In Users/yammer-sudden-drop-in-users.sql
+++ Yammer - Sudden Drop In Users/yammer-sudden-drop-in-users.sql
@@ -105,7 +105,17 @@ SELECT sub.location,
   WHERE sub.august_incidents > sub.july_incidents
 ORDER BY sub.august_incidents - sub.july_incidents DESC
 
-
+/* query 8 */
+SELECT EXTRACT('week' FROM occurred_at) AS week,
+  COUNT(DISTINCT CASE WHEN device IN ('macbook pro','lenovo thinkpad','macbook air','dell inspiron notebook',
+          'asus chromebook','dell inspiron desktop','acer aspire notebook','hp pavilion desktop','acer aspire desktop','mac mini')
+          THEN user_id ELSE NULL END) AS computer,
+  COUNT(DISTINCT CASE WHEN device IN ('iphone 5','samsung galaxy s4','nexus 5','iphone 5s','iphone 4s','nokia lumia 635',
+       'htc one','samsung galaxy note','amazon fire phone') THEN user_id ELSE NULL END) AS phone,
+  COUNT(DISTINCT CASE WHEN device IN ('ipad air','nexus 7','ipad mini','nexus 10','kindle fire','windows surface',
+        'samsumg galaxy tablet') THEN user_id ELSE NULL END) AS tablet
+  FROM tutorial.yammer_events
+  GROUP BY 1
 
 
 
